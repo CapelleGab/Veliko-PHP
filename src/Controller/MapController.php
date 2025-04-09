54b8b7120cdd85f3200a4fd26835457c8d196a6b
CapelleGab <gabin.capelle.pro@gmail.com>
2025-04-10 00:21:44 +0200

    Ajout du type Email dans le RegistrationFormType.php et ajout de la doc dans MapController.php

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -6,10 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 final class MapController extends AbstractController
 {
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route('/api/stations', name: 'api_stations')]
     public function getAllStations(): Response
     {
@@ -17,5 +29,4 @@ final class MapController extends AbstractController
         $response = $client->request('GET', 'https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=1471');
         return $this->json($response->toArray());
     }
-
 }
