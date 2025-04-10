e5fcd0b2a6a8c72d99e84ff1d328bffe18762e81
CapelleGab <gabin.capelle.pro@gmail.com>
2025-04-10 18:08:16 +0200

    ✨REFACTOR : Add webpack encore managment for css and javascript

---
+++ assets/js/map.js
@@ -0,0 +1,99 @@
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../styles/map.style.css'
+import personIconUrl from '../images/marker-icon.png';
+
+const personIcon = L.icon({
+    iconUrl: personIconUrl,
+    iconSize: [25, 41],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32]
+});
+
+const changeDefaultIcon = () => {
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
+}
+
+
+const loadMap = () => {
+    let map = L.map('map').setView([48.8566, 2.3522], 16);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    return map;
+}
+
+const run = () => {
+    let map = loadMap();
+
+    changeDefaultIcon()
+
+    try {
+        map.locate({ setView: true, maxZoom: 16 });
+
+        map.on('locationfound', (e) => {
+            L.marker(e.latlng, {icon: personIcon}).addTo(map).bindPopup(`
+                                <div>
+                                   <p><strong>Vous êtes là</strong></p>
+                                </div>
+                            `)
+        });
+
+        map.on('locationerror', (e) => {
+            console.error('Erreur de géolocalisation :', e.message);
+        });
+    } catch (error) {
+        console.error('Erreur lors de la tentative de géolocalisation :', error);
+    }
+
+    getStations(map)
+}
+
+const getStations = (map) => {
+    try {
+        fetch('/api/stations')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Erreur HTTP : ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                data.records.forEach(record => {
+                    const station = record.fields;
+
+                    if (station.coordonnees_geo) {
+                        const [lat, lon] = station.coordonnees_geo;
+
+                        L.marker([lat, lon]).addTo(map)
+                            .bindPopup(`
+                                <div>
+                                    <h2 style="text-align: center">${station.name}</h2>
+                                    <p><strong>🚲 Vélos mécaniques disponibles :</strong> ${station.mechanical}</p>
+                                    <p><strong>⚡️ Vélos électriques disponibles :</strong> ${station.ebike}</p>
+                                    <p><strong>🔓 Bornes libres :</strong> ${station.numdocksavailable}</p>
+                                </div>
+                            `);
+                    } else {
+                        console.warn('Coordonnées manquantes pour la station :', station);
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des stations :', error);
+            });
+    } catch (error) {
+        console.error('Erreur lors de l\'initialisation de la récupération des stations :', error);
+    }
+}
+
+
+run()
