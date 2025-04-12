f5a18405a989a6b36b5b7cc4507ea634b76e8be1
CapelleGab <gabin.capelle.pro@gmail.com>
2025-04-12 16:22:28 +0200

    🔧 FIX : Change color of user marker (#7)

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,14 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styles/map.style.css'
-import personIconUrl from '../images/marker-icon.png';
 
 const personIcon = L.icon({
-    iconUrl: personIconUrl,
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
-    iconAnchor: [16, 32],
-    popupAnchor: [0, -32]
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
 });
 
 const changeDefaultIcon = () => {
